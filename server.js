6507c1d830b3a4f68cd837e72a4101a68484919d
Axellerosh <lubi.axellerosh@gmail.com>
2024-11-02 22:33:14 +0800

    render fixing

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
+const PORT = process.env.PORT || 3000;
 
 app.prepare().then(() => {
   const server = createServer((req, res) => {
@@ -35,8 +36,8 @@ app.prepare().then(() => {
     });
   });
 
-  server.listen(3000, (err) => {
+  server.listen(PORT, (err) => {
     if (err) throw err;
-    console.log("> Ready on http://localhost:3000");
+    console.log(`> Ready on http://localhost:${PORT}`);
   });
 });
